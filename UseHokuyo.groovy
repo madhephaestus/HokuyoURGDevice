5c8e908e7402c8d657e4dae053c3a96636c50999
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-17 15:09:08 -0400

    Adding a sweep example

--- UseHokuyo.groovy
+++ UseHokuyo.groovy
@@ -1,1 +1,8 @@
-// code here
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.sdk.common.DeviceManager
+
+ScriptingEngine.gitScriptRun("https://github.com/madhephaestus/HokuyoURGDevice.git", "HokuyoURGDevice.groovy")
+
+def lidar  =DeviceManager.getSpecificDevice("lidar")
+
+def sweep = lidar.startSweep(-90, 90, 0.5) 
